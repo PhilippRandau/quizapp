e6d6b21a59e9a3cffe38ea905205980aadef45a9
Philipp Randau <philipp-randau@web.de>
2022-11-25 16:58:23 +0100

    add multible Question Topics function

--- script.js
+++ script.js
@@ -1,91 +1,66 @@
-let questions = [
-    {
-        "question": "Wie wird Star Wars auf Deutsch genannt?",
-        "answer_1": "Sternen Krieg",
-        "answer_2": "Krieg der Sterne",
-        "answer_3": "Krieg gegen die Sterne",
-        "answer_4": "Es gab nie einen offiziellen deutschen Namen",
-        "right_answer": 2
-    },
-    {
-        "question": "Wann ist Star Wars erschienen?",
-        "answer_1": "1977",
-        "answer_2": "1999",
-        "answer_3": "2005",
-        "answer_4": "1957",
-        "right_answer": 1
-    },
-    {
-        "question": "Wer ist der Erfinder?",
-        "answer_1": "Sebastian Schweinsteiger",
-        "answer_2": "George W. Bush",
-        "answer_3": "Keanu Reeves",
-        "answer_4": "George Lucas",
-        "right_answer": 4
-    },
-    {
-        "question": "An welches große Medienunternehmen wurden die Rechte 2012 verkauft?",
-        "answer_1": "Sony",
-        "answer_2": "Netflix",
-        "answer_3": "Disney",
-        "answer_4": "Amazon",
-        "right_answer": 3
-    },
-    {
-        "question": "Wie viele Triologien gibt es aktuell?(2022)",
-        "answer_1": "5",
-        "answer_2": "3",
-        "answer_3": "1",
-        "answer_4": "2",
-        "right_answer": 2
-    },
-
-
-
-];
-
 let currentQuestion = 0;
-let QuestionPoints = 0;
+let questionPoints = 0;
+
+let questionsTopic = '';
+
+
 
 function init() {
-    let lengthQuestions = document.getElementById('length-questions');
-    let currentQuestionOnCard = document.getElementById('current-question');
-    lengthQuestions.innerHTML = '';
-    lengthQuestions.innerHTML = questions.length;
-    currentQuestionOnCard.innerHTML = '';
-    currentQuestionOnCard.innerHTML = currentQuestion + 1;
+
+    createContent();
 
 
-    showCurrentQuestion();
     resetChoosedAnswer();
+    showCurrentQuestion();
+}
+
+
+function chooseQuestionTopic(topic) {
+    questionsTopic = topic;
+    showCurrentQuestion();
+
+    resetReplay();
+
 
 }
 
 
 function showCurrentQuestion() {
-    let question = questions[currentQuestion];
+
+    questionStatus();
+    let questionTopic = questionsTopic[currentQuestion];
     let questionText = document.getElementById('question-text');
     let answer1 = document.getElementById('answer_1');
     let answer2 = document.getElementById('answer_2');
     let answer3 = document.getElementById('answer_3');
     let answer4 = document.getElementById('answer_4');
 
-    questionText.innerHTML = question['question'];
-    answer1.innerHTML = question['answer_1'];
-    answer2.innerHTML = question['answer_2'];
-    answer3.innerHTML = question['answer_3'];
-    answer4.innerHTML = question['answer_4'];
+    questionText.innerHTML = questionTopic['question'];
+    answer1.innerHTML = questionTopic['answer_1'];
+    answer2.innerHTML = questionTopic['answer_2'];
+    answer3.innerHTML = questionTopic['answer_3'];
+    answer4.innerHTML = questionTopic['answer_4'];
+    progressBar();
+}
+
+function questionStatus() {
+    let lengthQuestions = document.getElementById('length-questions');
+    let currentQuestionOnCard = document.getElementById('current-question');
+    lengthQuestions.innerHTML = '';
+    lengthQuestions.innerHTML = questionsTopic.length;
+    currentQuestionOnCard.innerHTML = '';
+    currentQuestionOnCard.innerHTML = currentQuestion + 1;
 }
 
 
 function answer(Answer_X) {
-    let rightAnswer = questions[currentQuestion]["right_answer"];
+    let rightAnswer = questionsTopic[currentQuestion]["right_answer"];
     let NumberFromString = extractNumberFromString(Answer_X);
     let rightAnswerString = `answer_${rightAnswer}`;
     console.log(rightAnswerString);
     if (NumberFromString == rightAnswer) {
         document.getElementById(Answer_X).classList.add('bg-success');
-        QuestionPoints++;
+        questionPoints++;
 
     } else {
         document.getElementById(Answer_X).classList.add('bg-danger');
@@ -101,25 +76,34 @@ function extractNumberFromString(Answer_X) {
 }
 
 function nextQuestion() {
-    if (currentQuestion < questions.length - 1) {
+    if (currentQuestion < questionsTopic.length - 1) {
         currentQuestion++;
         init();
+        resetChoosedAnswer();
     } else {
         showFinalPoints();
     }
 
 
 
+
+    progressBar();
+
+
 }
 
 function resetChoosedAnswer() {
-    document.getElementById('next-question').disabled = true;
 
+    document.getElementById('next-question').disabled = true;
     for (let i = 1; i < 5; i++) {
         document.getElementById(`answer_${i}`).classList.remove('bg-success');
         document.getElementById(`answer_${i}`).classList.remove('bg-danger');
         document.getElementById(`answer_${i}`).disabled = false;
     }
+
+
+
+
 
 } function lockAnswerButtons() {
     for (let i = 1; i < 5; i++) {
@@ -137,7 +121,7 @@ function showFinalPoints() {
     
         <h5 class="card-title mt-2 mb-2" id="question-text"> <b> Star Wars Quiz Erledigt </b></h5>
         <div class="result-points">
-            <span class="result-points-text" > Deine Punktzahl ist </span> <span> <b> ${QuestionPoints}/${questions.length} </b></span>
+            <span class="result-points-text" > Deine Punktzahl ist </span> <span> <b> ${questionPoints}/${questionsTopic.length} </b></span>
         </div>
         <button type="button" class="mt-4 btn btn-primary btn-width">Share</button>
         <button onclick="resetReplay()" type="button" class="mt-2 btn btn-outline-primary btn-width">Replay</button>
@@ -146,15 +130,84 @@ function showFinalPoints() {
     document.getElementById('tropy').classList.remove('d-none');
 }
 
-function resetReplay(){
+function resetReplay() {
     hideTropyIMG();
     resetPoints();
+    resetCurrentQuestion();
 }
 
-function hideTropyIMG(){
+function hideTropyIMG() {
     document.getElementById('tropy').classList.add('d-none');
 }
 
-function resetPoints(){
-    QuestionPoints = 0;
+function resetPoints() {
+    questionPoints = 0;
+
+}
+function resetCurrentQuestion() {
+    currentQuestion = 0;
+}
+
+function progressBar() {
+    let progress = 100 / questionsTopic.length * (currentQuestion + 1)
+
+    document.getElementById('progress-bar').style.width = `${progress}%`;
+
+}
+
+function createContent() {
+    let content = document.getElementById('content');
+    content.innerHTML = /*html*/`
+    <div class="card-body" id="card-body">
+
+    <h5 class="card-title mt-2" id="question-text">Frage</h5>
+    <div>
+        <div class="mt-4 mb-3 answer-buttons-body">
+            <button class="answer-buttons " id="answer_1" onclick="answer('answer_1')">
+                Antwort 1
+            </button>
+        </div>
+
+        <div class="mb-3 answer-buttons-body">
+            <button class="answer-buttons " id="answer_2" onclick="answer('answer_2')">
+                Antwort 2
+            </button>
+        </div>
+
+        <div class="mb-3 answer-buttons-body">
+            <button class="answer-buttons " id="answer_3" onclick="answer('answer_3')">
+                Antwort 3
+            </button>
+        </div>
+
+        <div class="mb-3 answer-buttons-body">
+            <button class="answer-buttons " id="answer_4" onclick="answer('answer_4')">
+                Antwort 4
+            </button>
+        </div>
+    </div>
+
+
+
+    <div class="card-navigation">
+            <button id="previous-question" class="pl-16 btn btn-primary previous-question"
+                onclick="nextQuestion(), resetChoosedAnswer()" href="#" disabled>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                    <path
+                        d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                </svg>
+            </button>
+            <span> <b id="current-question">1</b> von <b id="length-questions">5</b> Fragen</span>
+            <button id="next-question" class="btn btn-primary next-question" onclick="nextQuestion()"
+                href="#" disabled>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                    <path
+                        d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                </svg>
+            </button>
+        </div>
+    </div>
+                `;
 }
---
+++ templates/database.js
@@ -0,0 +1,110 @@
+let questionsSW = [
+    {
+        "question": "Wie wird Star Wars auf Deutsch genannt?",
+        "answer_1": "Sternen Krieg",
+        "answer_2": "Krieg der Sterne",
+        "answer_3": "Krieg gegen die Sterne",
+        "answer_4": "Es gab nie einen offiziellen deutschen Namen",
+        "right_answer": 2
+    },
+    {
+        "question": "Wann ist Star Wars erschienen?",
+        "answer_1": "1977",
+        "answer_2": "1999",
+        "answer_3": "2005",
+        "answer_4": "1957",
+        "right_answer": 1
+    },
+    {
+        "question": "Wer ist der Erfinder?",
+        "answer_1": "Sebastian Schweinsteiger",
+        "answer_2": "George W. Bush",
+        "answer_3": "Keanu Reeves",
+        "answer_4": "George Lucas",
+        "right_answer": 4
+    },
+    {
+        "question": "An welches große Medienunternehmen wurden die Rechte 2012 verkauft?",
+        "answer_1": "Sony",
+        "answer_2": "Netflix",
+        "answer_3": "Disney",
+        "answer_4": "Amazon",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie viele Triologien gibt es aktuell?(2022)",
+        "answer_1": "5",
+        "answer_2": "3",
+        "answer_3": "1",
+        "answer_4": "2",
+        "right_answer": 2
+    },
+];
+
+let questionsPl = [
+    {
+        "question": "Wie viele Planeten gehören zu unserem Sonnensystem?",
+        "answer_1": "10",
+        "answer_2": "5",
+        "answer_3": "8",
+        "answer_4": "7",
+        "right_answer": 3
+    },
+    {
+        "question": "Mit welcher Geschwindigkeit bewegt sich Merkur um die Sonne?",
+        "answer_1": "50 km/s",
+        "answer_2": "100 km/s",
+        "answer_3": "50 km/min",
+        "answer_4": "50 km/h",
+        "right_answer": 1
+    },
+    {
+        "question": "Zählt Pluto als Planet zu unserem Sonnensystem?",
+        "answer_1": "Ja",
+        "answer_2": "Ja, aber bald nicht mehr.",
+        "answer_3": "Nein, zu weit weg",
+        "answer_4": "Nein, zu klein, aber er zählte mal dazu",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie lange braucht die Erde, um einmal um die Sonne zu kreisen?",
+        "answer_1": "ungefähr 360 Tage",
+        "answer_2": "ungefähr 365 Tage",
+        "answer_3": "ungefähr 265 Tage",
+        "answer_4": "ungefähr 188 Tage",
+        "right_answer": 2
+    },
+    {
+        "question": "Warum ist der Mars rot?",
+        "answer_1": "Wegen des hohen Eisenanteils -> Rost",
+        "answer_2": "Wegen sehr hohen Temperaturen",
+        "answer_3": "Algen färben ihn",
+        "answer_4": "Er ist in Wirklichkeit garnicht rot",
+        "right_answer": 1
+    },
+    {
+        "question": "Was für ein Planet ist Neptun?",
+        "answer_1": "Eisplanet",
+        "answer_2": "Blauer Riese",
+        "answer_3": "Wasserplanet",
+        "answer_4": "Gasplanet",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche Temperatur hat die Venus auf der Obefläche?",
+        "answer_1": "-255°C",
+        "answer_2": "10°C-20°C",
+        "answer_3": "400°C-500°C",
+        "answer_4": "-100°C-200°C",
+        "right_answer": 3
+    },
+    {
+        "question": "Welcher Planet hat einen Ring um sich?",
+        "answer_1": "Mond",
+        "answer_2": "Saturn",
+        "answer_3": "Venus",
+        "answer_4": "Neptun",
+        "right_answer": 2
+    },
+
+];
